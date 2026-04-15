0442e5c0b8f4576088f420505c2411043843a6dc
Gabrovez <gabrovez321@gmail.com>
2026-04-15 09:04:21 +0300

    Add background color and gradient

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,57 @@ void main() {
     MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold (
-         body: Center(child: Text("Hello world!")),
+         body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.white,
+                Colors.blue,
+                Colors.red,
+              ],
+            ),
+          ),
+           child: Center(
+            child: Text("Hello world!"),
+                   ),
+         ),
       ),
     ),
   );
 }
+
+// шаг 7 
+// void main() {
+//   runApp(
+//     MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold (
+//         backgroundColor: const Color.fromARGB(255, 112, 211, 115),
+//          body: Center(
+//           child: Text("Hello world!"),
+//         ),
+//       ),
+//     ),
+//   );
+// }
+
+//step 6
+
+// void main() {
+//   runApp(
+//     MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold (
+//          body: Center(child: Text("Hello world!")),
+//       ),
+//     ),
+//   );
+// }
+
+
+// step 4
+
+// void main() {
+//   runApp(MaterialApp(home: Text("Hello world!")));
+// }
+
